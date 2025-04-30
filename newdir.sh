13dec850d2ca4d232dca8fa02eb10a618abc3908
ChaouiZ <ztilos713@gmail.com>
2025-04-30 09:27:22 -0500

    updated newdir script for portability

--- newdir.sh
+++ newdir.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 dirname = $1
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mkdir /home/zach/code/impressions/$1
-cp /home/zach/code/impressions/{count,catchup}.js $1
-cp /home/zach/code/impressions/{impressions,catchupimpressions}.txt $1
 
-echo "/home/zach/code/impressions/$1/ has been created"
+mkdir $SCRIPT_DIR/$1
+cp $SCRIPT_DIR/{count,catchup}.js $1
+cp $SCRIPT_DIR/{impressions,catchupimpressions}.txt $1
+
+echo "$SCRIPT_DIR/$1/ has been created"
